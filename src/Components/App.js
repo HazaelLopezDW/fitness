12f41ea7ee8726d5b48bcafea690bd9956c32a54
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 08:22:25 -0600

    Estamos creando un componente funcional

---
+++ src/Components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { BrouserRouter, Route, Switch} from "react-router-dom"
+
+const App = () => (
+    <BrouserRouter>
+        <Route path="" component={}/>
+    </BrouserRouter>
+)
+
+export default App
