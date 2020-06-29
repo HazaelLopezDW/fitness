9e341e9a6080816851f66edbe4fe096710b9a432
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 11:50:46 -0600

    Elevando el estado

--- src/Components/ExerciseForm.js
+++ src/Components/ExerciseForm.js
@@ -3,39 +3,35 @@ import Exercises from "../pages/Exercises"
 
 class ExerciseForm extends React.Component{
 
-    state = {}
 
     handleSubmit = ev =>{
         ev.preventDefault()
         console.log(this.state)
     }
 
-    handleChange = ev =>{
-        this.setState({
-            [ev.target.name]: ev.target.value
-        })
-
-    }
 
     render(){
+
+        const {onChange, form} = this.props
+
         return(
             <div className="container">
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="title" name="title"onChange={this.handleChange} value={this.state.title} />
+                    <input type="text" className="form-control" placeholder="title" name="title"onChange={onChange} value={form.title} />
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="description" name="description" onChange={this.handleChange} value={this.state.description} />
+                    <input type="text" className="form-control" placeholder="description" name="description" onChange={onChange} value={form.description} />
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="img" name="img" onChange={this.handleChange} value={this.state.img} />
+                    <input type="text" className="form-control" placeholder="img" name="img" onChange={onChange} value={form.img} />
                 </div>
                 <div className="form-row">
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="leftColor" name="leftColor" onChange={this.handleChange} value={this.state.leftColor} />
+                        <input type="text" className="form-control" placeholder="leftColor" name="leftColor" onChange={onChange} value={form.leftColor} />
                     </div>
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="rightColor" name="rightColor" onChange={this.handleChange} value={this.state.rightColor} />    
+                        <input type="text" className="form-control" placeholder="rightColor" name="rightColor" onChange={onChange} value={form.rightColor} />    
                     </div>
                 </div>
                 <br/>
--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -3,9 +3,42 @@ import ExerciseForm from "../Components/ExerciseForm"
 import Card from "../Components/Card"
 
 class ExerciseNew extends React.Component{
+
+    state = {
+        form: {
+            title: "",
+            description: "",
+            img: "",
+            leftColor: "",
+            rightColor: ""
+        }
+    }
+
+    handleChange = ev =>{
+        this.setState({
+            form: {
+                ...this.state.form,
+                [ev.target.name]: ev.target.value
+            }
+        })
+
+    }
+
     render(){
         return(
-            <ExerciseForm/>
+            <div className="row">
+                <div className="col-sm">
+                    <Card
+                        {...this.state.form}
+                    />
+                </div>
+                <div className="col-sm">
+                    <ExerciseForm
+                        onChange={this.handleChange}
+                        form={this.state.form}
+                    />
+                </div>
+            </div>
         )
     }
 }
