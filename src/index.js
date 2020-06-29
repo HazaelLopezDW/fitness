2db1137c87efb10a798db894af24918f7cbd67c9
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 21:48:33 -0600

    Borramos las importaciones de nuestros componentes Card.js y Welcome.js Cortamos nuestro codigo de Card y Welcome para llevarlo a Exercises.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Card from "./Components/Card"
-import Welcome from "./Components/Welcome"
+import Exercises from "./pages/Exercises"
 
 // Creamos nuestro contenedor
 const container = document.getElementById('root')
 
-ReactDOM.render(
-                <React.Fragment>
-                  <Welcome
-                    userName = "Hazael López Díaz"
-                  />
-                  <Card
-                      title = "Es texto no Importa"
-                      description = "Porque consumiremos recursos de una API"
-                      img = "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-                      leftColor = "#23aabc"
-                      rightColor = "#67FF37"
-                  />
-                </React.Fragment>, container)
+ReactDOM.render(<Exercises/>, container)
