f40dc5fbb5a746e6d987f8190ccd5b82da0b4927
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 21:45:21 -0600

    Creamos un componente de clase he importamos nuestros componentes Card.js y Welcome.js

---
+++ src/pages/Exercises.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Card from "../Components/Card"
+import Welcome from "../Components/Welcome"
+
+class Exercises extends React.Component{
+
+}
+
+export default Exercises
