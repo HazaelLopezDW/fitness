7d2bd7bedb0a04776afcd6a23d7247d10c0f1612
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 19:16:39 -0600

    Agregamos algunas clases desde nuestros estilos Card.css y desde bootstrap

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,22 @@
 import React from "react"
 import exerciseImg from "../images/exercise.png"
+import "./styles/Card.css"
 
 class Card extends React.Component{
 
     render(){
         return(
-            <div>
-                <div>
-                    <img src={exerciseImg}/>
-                </div>
-                <div>
-                    <h1>Es texto no Importa</h1>
-                    <p>Porque consumiremos recursos de una API</p>
+            <div className="card mx-auto Fitness-Card">
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={exerciseImg} className="float-right"/>
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>Es texto no Importa</h1>
+                            <p>Porque consumiremos recursos de una API</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
