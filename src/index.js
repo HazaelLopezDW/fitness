8bce5f73d8a6f4c283c32debf5e51a9139da4469
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 18:29:01 -0600

    Vemos como es que se renderizan los componentes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
 
+// Creamos un Contenedor y un componente para nuestra aplicacion 
+
+const component = <h1> hola mundo</h1>
+const container = document.getElementById('root')
+
+ReactDOM.render(component, container)
