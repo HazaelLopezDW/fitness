f666f1a4630294759c95f4b928b2843f1e339c1d
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 21:28:32 -0600

    Importamos nuestros estilos y creamos un mensaje

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,8 +1,12 @@
 import React from "react"
+import "./styles/Welcome.css"
 
 const Welcome = () =>(
-    <div>
-        
+    <div className="container">  
+        <div className="Fitness-User-Info">
+            <h1>Hola Hazael López</h1>
+            <p>Tienes que confiar en Dios Hazael!</p>
+        </div>        
     </div>
 )
 
