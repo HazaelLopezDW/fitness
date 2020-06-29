b7c16ea80e50ccfa34b72a5f2debaa266d4865ad
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 09:19:59 -0600

    Estamos haciendo uso de Babel con funciones de flecha y destructuracion

--- src/Components/ExerciseList.js
+++ src/Components/ExerciseList.js
@@ -1,26 +1,24 @@
 import React from "react"
 import Card from "../Components/Card"
 
-const ExerciseList = ({exercises}) =>{
-    return(
-        <div>
-            {exercises.map((exercise) =>{
-                return(
-                    <div>
+const ExerciseList = ({exercises}) =>(
+    <div>
+        {exercises.map((exercise) =>{
+            return(
+                <div>
                     <Card
                         title = {exercise.title}
                         description = {exercise.description}
                         img = {exercise.img}
                         leftColor = {exercise.leftColor}
                         rightColor = {exercise.rightColor}
-                />
+                    />
                 <br/>
                 </div>
-                )
-            })}
-        </div>
-    )   
-}
+            )
+        })}
+    </div>
+) 
 
 
 export default ExerciseList
