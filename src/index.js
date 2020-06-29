a63ed488f532fb5ab401a422afbcb74635b88e9b
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 20:09:09 -0600

    Le mandamos las propiedades a nuestro componente Card.js por medio de la importacion y el componente que mandamos a hacer render en el metodo render()

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ import Card from "./Components/Card"
 // Creamos nuestro contenedor
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card/>, container)
+ReactDOM.render(<Card
+                    title = "Es texto no Importa"
+                    description = "Porque consumiremos recursos de una API"
+                    img = "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                    leftColor = "#23aabc"
+                    rightColor = "#67FF37"
+                />, container)
