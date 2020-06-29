29d0901124a76bab6113dfa96b12f046842323d2
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 18:50:52 -0600

    Empezamos a contruir nuestro Componente de carta

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,15 @@ class Card extends React.Component{
 
     render(){
         return(
-            <h1>Hola Walter Hazael</h1>
+            <div>
+                <div>
+                    <img />
+                </div>
+                <div>
+                    <h1>Es texto no Importa</h1>
+                    <p>Porque consumiremos recursos de una API</p>
+                </div>
+            </div>
         )
     }
 }
