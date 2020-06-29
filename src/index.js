3003a25506363a0ac812ddac725b4794bc4ad321
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 08:19:08 -0600

    instalamos react-router-dom para usarlo en nuestro proyecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Exercises from "./pages/Exercises"
+import App from "./Components/App"
 
 // Creamos nuestro contenedor
 const container = document.getElementById('root')
