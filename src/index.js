a20a062881bcef1b909b257cc1be40e7b715f2af
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 08:23:59 -0600

    Importamos a nuestro componente App y hacemos render del mismo

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import App from "./Components/App"
 // Creamos nuestro contenedor
 const container = document.getElementById('root')
 
-ReactDOM.render(<Exercises/>, container)
+ReactDOM.render(<App/>, container)
