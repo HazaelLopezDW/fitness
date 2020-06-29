d5b7efc33430348f3c3d437829c1ef2715e333ca
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 08:32:09 -0600

    Creamos nuestro componente de clase Excercise

---
+++ src/pages/ExerciseNew.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Exercises from "./Exercises"
+
+class Exercise extends React.Component{
+    render(){
+        return(
+            <h1>Hola mundo!!!!</h1>
+        )
+    }
+}
+
+export default Exercise
