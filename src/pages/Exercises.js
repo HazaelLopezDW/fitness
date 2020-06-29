34c36388844ed88a1dc9b587e03c13a6130565d7
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 21:50:28 -0600

    Creamos nuestro metodo render y retornamos Welcome y Card con sus propiedades

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -3,7 +3,22 @@ import Card from "../Components/Card"
 import Welcome from "../Components/Welcome"
 
 class Exercises extends React.Component{
-
+    render(){
+        return(
+            <React.Fragment>
+            <Welcome
+              userName = "Hazael López"
+            />
+            <Card
+                title = "Es texto no Importa"
+                description = "Porque consumiremos recursos de una API"
+                img = "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                leftColor = "#23aabc"
+                rightColor = "#67FF37"
+            />
+          </React.Fragment>
+        )
+    }
 }
 
 export default Exercises
