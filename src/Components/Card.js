8ea19513a605e018ce113594f03dbfca2a058fcc
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 19:57:08 -0600

    Importamos nuestra imagen de circulos y crearemos estilos en linea para hacer usos de propiedades 'props'

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,18 @@
 import React from "react"
 import exerciseImg from "../images/exercise.png"
+import circlesImg from "../images/circles.png"
 import "./styles/Card.css"
 
 class Card extends React.Component{
 
     render(){
         return(
-            <div className="card mx-auto Fitness-Card">
+            /*background: url('../../images/circles.png') no-repeat, linear-gradient(to right, #A74CF2, #617BFB);*/
+            <div className="card mx-auto Fitness-Card"
+                style = {{
+                    backgroundImage: ``
+                }}
+            >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
