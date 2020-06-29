cf003a1b437d0cd6cbd544a0d4c8905eed923e89
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 09:23:54 -0600

    Agregamos una key

--- src/Components/ExerciseList.js
+++ src/Components/ExerciseList.js
@@ -7,6 +7,7 @@ const ExerciseList = ({exercises}) =>(
             return(
                 <div>
                     <Card
+                        key={exercise.id}
                         title = {exercise.title}
                         description = {exercise.description}
                         img = {exercise.img}
