f28f9b3628b8c4221e7268a29ec0735a58174b65
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 15:42:39 -0600

    Mejoramos nuestro 400.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React from 'react'
+import '../components/styles/Errors.css'
+import NotFoundImg from '../images/404.png'
 
-const NotFount = () =>(
-    <h1>Pagína no encontrada</h1>
+const NotFount = () => (
+    <div className="text-center">
+        <h1 className="Error_Text">Error: 404 Page Not Found</h1>    
+        <img src={NotFoundImg} alt="404 NotFound" className="Error_Image" />
+    </div>
 )
 
 export default NotFount
