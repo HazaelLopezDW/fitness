40d1eb76822395f02f1a9f119a3958dda0d84e9f
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 08:13:51 -0600

    Arreglamos los problemas de estilos que tenia nuestro componente

--- src/Components/AddButton.js
+++ src/Components/AddButton.js
@@ -3,9 +3,9 @@ import addImg from "../images/add.png"
 import "./styles/AddButton.css"
 
 const AddButton = () =>(
-    <div>
-        <img src={addImg}/>
+    <div >
         <a href="exercises/new"></a>
+        <img src={addImg} className="Fitness-Add"/>
     </div>
 )
 
