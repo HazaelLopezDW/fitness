caf307ffa3e1d07d1085f818e37c2c1da14fb935
Hazael Lopez <elhazael134@gmail.com>
2020-06-29 08:25:46 -0600

    Importamos Exercises.js a nuestro componente App

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { BrouserRouter, Route, Switch} from "react-router-dom"
+import Exercises from "./pages/Exercises"
 
 const App = () => (
     <BrouserRouter>
-        <Route path="" component={}/>
+        <Route path="" component={Exercises}/>
     </BrouserRouter>
 )
 
