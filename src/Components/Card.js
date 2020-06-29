8cbaeb9172ec15347256ba8a805dbbf6cedddce5
Hazael Lopez <elhazael134@gmail.com>
2020-06-28 18:39:54 -0600

    Creamos un componente de clases

---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Card extends React.Component{
+
+    render(){
+        return(
+            <h1>Hola Walter Hazael</h1>
+        )
+    }
+}
+
+export default Card
