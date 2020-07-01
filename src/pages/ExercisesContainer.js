397a4dce3948f459de5e616fae28ca09a6fd9e3b
Hazael Lopez <elhazael134@gmail.com>
2020-07-01 15:36:03 -0600

    Simplemente borramos las lineas comentadas

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -38,49 +38,5 @@ const ExercisesContainer = () =>{
         </React.Fragment>    
     )
 }
-/*class ExercisesContainer extends React.Component{
-
-    state = {
-            data: [],
-            Loading: true,
-            error: null
-        }
-
-        async componentDidMount(){
-            await this.fetchExercises()
-        }
-
-        fetchExercises = async () =>{
-            try {
-                let res = await fetch('http://localhost:8000/api/exercises/')
-                let data = await res.json()
-
-                this.setState({
-                    data, 
-                    Loading: false
-                })     
-            } catch (error) {
-                this.setState({
-                    Loading: false,
-                    error
-                })
-            }
-        }
-
-
-    render(){
-        if(this.state.Loading)
-            return<Loading/>
-        if(this.state.error)
-            return<Error/>
-        return(
-        <React.Fragment>
-            <Exercises
-                data={this.state.data}
-            />
-        </React.Fragment>
-        )
-    }
-}*/
 
 export default ExercisesContainer
