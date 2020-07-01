9484290aea3ee49abc3743934a40f76a43e689f4
Hazael Lopez <elhazael134@gmail.com>
2020-07-01 15:27:02 -0600

    Haciendo uso de hook useEffect

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Exercises from "./Exercises"
 import Loading from "../Components/Loading"
 import Error from "./500"
