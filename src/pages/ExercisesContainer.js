b7fe69797ceb959069a16b46eb2a642128b4d6a7
Hazael Lopez <elhazael134@gmail.com>
2020-07-01 15:46:34 -0600

    Cortamos este codigo para llevarlo a src/hooks/useFetch.js

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -5,26 +5,6 @@ import Loading from "../Components/Loading"
 import Error from "./500"
 
 const ExercisesContainer = () =>{
-    const [ data, setData ] = useState([])
-    const [ loading, setLoading ] = useState(false)
-    const [ error, setError ] = useState(null)
-
-    useEffect( () =>{
-        const fetchExercises = async () =>{
-            try {
-                let res = await fetch('http://localhost:8000/api/exercises/')
-                let data = await res.json()
-
-                setData(data)
-                setLoading(false)
-
-            } catch (error) {
-                setLoading(false)
-                setError(error)
-            }
-        }
-        fetchExercises()
-    }, [])
 
     if(loading)
         return<Loading/>
