13e2543e6690a6c80a1d93bfecb023a326a413ee
Hazael Lopez <elhazael134@gmail.com>
2020-07-01 12:50:45 -0600

    Haciendo usos de los hook useState()

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,8 +1,26 @@
 import React from "react"
+import { useState } from "react"
 import Exercises from "./Exercises"
 import Loading from "../Components/Loading"
 import Error from "./500"
 
+const ExercisesContainer = () =>{
+    const [ data, setData ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(null)
+
+    if(loading)
+        return<Loading/>
+    if(error)
+        return<Error/>
+    return(
+        <React.Fragment>
+            <Exercises
+                data={data}
+            />
+        </React.Fragment>    
+    )
+}
 /*class ExercisesContainer extends React.Component{
 
     state = {
